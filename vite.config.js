b843132414efdf29a7d9b1eef58ab21520df1792
islemk69 <islemkaismoun@gmail.com>
2025-07-05 18:46:14 +0200

    add deploy github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/nom-du-repo/', // ← remplace par le nom exact de ton repo GitHub
+  base: '/megalio/',
 })
